8471edd6f61371f1feb706a799d28b4a97441034
manager <kaduce2009@hotmail.com>
2019-09-18 22:13:18 -0300

    vai Deus

---
+++ app/AppColorido-master/app/src/main/java/br/com/etecia/appcolorido/pag2_Activity.java
@@ -0,0 +1,13 @@
+package br.com.etecia.appcolorido;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class pag2_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pag2_layout);
+    }
+}
